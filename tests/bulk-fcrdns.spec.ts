68dd5a4d6e9ea5d4903482a2da18c0ddf57b283b
Francis <FrancisPBaker@gmail.com>
2023-12-04 19:25:51 -0500

    Update bulk-fcrdns.spec.ts

--- tests/bulk-fcrdns.spec.ts
+++ tests/bulk-fcrdns.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('has main here', async ({ page }) => {
   await page.goto('http://localhost:3000/tools/bulk-fcrdns');
-  const textElement = await page.$(`text="Bulk FCrDNS Check"`);
+  const textElement = await page.$(`text="Bulk FCrDNS Lookup"`);
   expect(textElement).not.toBeNull();
 });
