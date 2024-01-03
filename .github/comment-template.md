04f2c37e710ed7f2faf793d5e48528d36fb06c6a
Francis <9560564+Baker@users.noreply.github.com>
2024-01-03 15:04:38 -0500

    x

--- .github/comment-template.md
+++ .github/comment-template.md
@@ -2,4 +2,4 @@ Howdy 🖐 {{ .author }}!
 
 Here is the latest [Deployment URL]({{.url}}). This comment was last updated on _{{.time}}_.
 
-_Please know this was automatically posted._
+_Please know this was automatically posted and updated._
--- next.config.js
+++ next.config.js
@@ -18,7 +18,64 @@ const nextConfig = {
   sentry: {
     hideSourceMaps: true,
   },
+  headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
+  },
 };
+
+const cspHeader = `
+    default-src 'self' vitals.vercel-insights.com vercel.live static.cloudflareinsights.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' vercel.live static.cloudflareinsights.com;
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
 
 const SentryWebpackPluginOptions = {
   silent: true,
