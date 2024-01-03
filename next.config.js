20ff81b59873bcf11b5ee3c7e5926dde3dadcbc1
Francis <9560564+Baker@users.noreply.github.com>
2024-01-03 17:03:02 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,6 +18,17 @@ const nextConfig = {
   sentry: {
     hideSourceMaps: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Headers', value: 'sentry-trace' },
+          { key: 'Access-Control-Allow-Headers', value: 'baggage' },
+        ],
+      },
+    ];
+  },
 };
 
 const SentryWebpackPluginOptions = {
